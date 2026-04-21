41e6ccd86c685d5fe0edc4496950c9a4a895bba3
Tesalriah <proella8@gmail.com>
2026-04-21 17:43:43 +0900

    t3, t4 ok

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,17 +1,22 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 public class ApplicationContext {
     private TestPostService testPostService;
+    private TestPostRepository testPostRepository;
 
     public ApplicationContext() {
         testPostService = new TestPostService();
+        testPostRepository = new TestPostRepository();
     }
 
     public <T> T genBean(String beanName) {
         if(beanName.equals("testPostService")){
             return (T)testPostService;
+        }if(beanName.equals("testPostRepository")){
+            return (T)testPostRepository;
         }
         return (T) null;
     }
