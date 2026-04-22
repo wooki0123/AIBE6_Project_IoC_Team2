8011d728db98d6fe05ef800ec2431f184b3541b9
ysman <tke0329@gmail.com>
2026-04-22 16:46:14 +0900

    0000/프로젝트셋팅_및_완료

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,34 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+    private final TestPostRepository testPostRepository;
+    private final TestPostService testPostService;
+    private final TestFacadePostService testFacadePostService;
+
+    Map<String, Object> beanMap;
+
+    public ApplicationContext() {
+        this.testPostRepository = new TestPostRepository();
+        this.testPostService = new TestPostService(testPostRepository);
+        this.testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
+        this.beanMap = new HashMap<>();
+
+        beanMap.put("testPostRepository", testPostRepository);
+        beanMap.put("testPostService", testPostService);
+        beanMap.put("testFacadePostService", testFacadePostService);
     }
+
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return (T) beanMap.get(beanName);
     }
+
+
 }
