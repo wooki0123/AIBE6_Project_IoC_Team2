5a966455f02b45d1724c5dd31c1640886d3372fd
wooki0123 <wookimini22@gmail.com>
2026-04-22 16:12:12 +0900

    ãfeat: 테스트케이스 t6까지 진행

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,27 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+    private Map<String, Object> beans = new HashMap<>();
+
+    public ApplicationContext() {
+        TestPostRepository testPostRepository = new TestPostRepository();
+        beans.put("testPostRepository", testPostRepository);
+
+        TestPostService testPostService = new TestPostService(testPostRepository);
+        beans.put("testPostService", testPostService);
+
+        TestFacadePostService testFacadePostService = new TestFacadePostService(testPostService, testPostRepository);
+        beans.put("testFacadePostService", testFacadePostService);
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return (T) beans.get(beanName);
     }
 }
