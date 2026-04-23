9d4e5c856dd05b195ca4e76ee6305008dd509dd9
Kim Juyoung <op45672@gmail.com>
2026-04-23 16:39:04 +0900

    기초세팅 및 v2테스트통과

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,18 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
     public ApplicationContext() {
-
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        if (beanName.equals("testPostService")) {
+            TestPostRepository testPostRepository = new TestPostRepository();
+            return (T) new TestPostService(testPostRepository);
+        }
+
+        return null;
     }
 }
