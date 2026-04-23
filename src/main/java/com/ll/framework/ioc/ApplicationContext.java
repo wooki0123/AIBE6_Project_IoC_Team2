8932345b3dfa344bd759a1429f00d23e743b7447
Kim Juyoung <op45672@gmail.com>
2026-04-23 16:54:48 +0900

    t6 테스트 통과

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,14 @@
 package com.ll.framework.ioc;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 public class ApplicationContext {
     private TestPostRepository testPostRepository;
     private TestPostService testPostService;
+    private TestFacadePostService testFacadePostService;
+
     public ApplicationContext() {
     }
 
@@ -22,6 +25,13 @@ public class ApplicationContext {
                 testPostService=new TestPostService(genBean("testPostRepository"));
             }
             return (T) testPostService;
+        }
+
+        if (beanName.equals("testFacadePostService")) {
+            if(testFacadePostService==null){
+                testFacadePostService=new TestFacadePostService(genBean("testPostService"),genBean("testPostRepository"));
+            }
+            return (T) testFacadePostService;
         }
 
         return null;
