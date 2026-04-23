a69676a319b92ddf2c83be7259aea145507634ee
Kim Juyoung <op45672@gmail.com>
2026-04-23 16:48:02 +0900

    v3 테스트 통과

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -4,13 +4,17 @@ import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 public class ApplicationContext {
+    private TestPostService testPostService;
     public ApplicationContext() {
     }
 
     public <T> T genBean(String beanName) {
         if (beanName.equals("testPostService")) {
-            TestPostRepository testPostRepository = new TestPostRepository();
-            return (T) new TestPostService(testPostRepository);
+            if(testPostService==null){
+                TestPostRepository testPostRepository = new TestPostRepository();
+                testPostService=new TestPostService(testPostRepository);
+            }
+            return (T) testPostService;
         }
 
         return null;
